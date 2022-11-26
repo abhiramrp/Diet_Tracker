677e47680b11baed922dcf3be7c4c9e3d533f9d8
Abhiram Rishi Prattipati <pabhiramrishi@gmail.com>
2022-11-25 22:37:30 -0800

    Added user email/password

--- app/src/main/java/com/example/diet_tracker/MainActivity.kt
+++ app/src/main/java/com/example/diet_tracker/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.diet_tracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var analytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Obtain the FirebaseAnalytics instance.
+        analytics = Firebase.analytics
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun signOut() {
+        // [START auth_sign_out]
+        Firebase.auth.signOut()
+        // [END auth_sign_out]
     }
 }
